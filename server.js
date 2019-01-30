ed793e27da587e97f1556eda18174efbd639edac
Nikola Milicevic <nikolamilicevic99@yahoo.com>
2019-01-30 23:37:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,17 @@
-'use strict';
-
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-
-var cors = require('cors');
-
+var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+app.use(bodyParser.json());
 
-// Basic Configuration 
-var port = process.env.PORT || 3000;
-
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
-
-app.use(cors());
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
-
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/", function(req, res){
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/shorturl/new", function(req, res){
+  console.log("OK");
 });
 
-
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+app.listen(3000, function(){
+  console.log("Working");
 });
