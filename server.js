3445c52ff03a0ee0397c846823d60c9a578ac1de
Nikola Milicevic <nikolamilicevic99@yahoo.com>
2019-03-27 20:00:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,57 @@
-var express = require("express");
-var app = express();
+'use strict';
+
+var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+
+var cors = require('cors');
+
+var app = express();
+
+// Basic Configuration 
+var port = process.env.PORT || 3000;
+
+/** this project needs a db !! **/ 
+mongoose.connect(process.env.MONGOLAB_URI);
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use("/", function(req, res){
-  res.sendFile(__dirname + "/views/index.html");
+var Schema = mongoose.Schema;
+var urlSchema = new Schema({
+  "url": String,
+  shortUrl: Number
+});
+var urlDatabase = mongoose.model("urlDatabase", urlSchema);
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+
+
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.get("/api/shorturl/new", function(req, res){
-  console.log("OK");
+// your first API endpoint... 
+app.post("/api/shorturl/new", function (req, res) {
+  var shortUrlNumber  = Math.floor(Math.random() * 1000);
+  
+  var myURL = new urlDatabase(req.body, shortUrlNumber);
+  
+  
+  myURL.save().then((data) => {
+    res.json({url: myURL, shortURL: shortUrlNumber});
+  }).catch((err) => {
+    res.status(400).json({message: "Cant write data"});
+  });
 });
 
-app.listen(3000, function(){
-  console.log("Working");
+
+app.listen(port, function () {
+  console.log('Node.js listening ...');
 });
